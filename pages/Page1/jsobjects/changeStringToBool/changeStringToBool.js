cdf68fcb3852348dbb7d093a7392acf1cab205ba
Cody Launius <claunius@ztelco.com>
2024-04-19 15:26:45 +0000

    disable next button on Assistance requesst add string to bool js object

---
+++ pages/Page1/jsobjects/changeStringToBool/changeStringToBool.js
@@ -0,0 +1,7 @@
+export default {
+	household_dataCopyschema__root_schema__childrendisabilitychildrenstatusonSelectionChange () {
+	household_dataCopy.formData.disability.status = (household_dataCopy.formData.disability.status==1) ? true : false 
+	household_dataCopy.formData.disability.ssiapplied = (household_dataCopy.formData.disability.ssiapplied==1) ? true : false 
+	household_dataCopy.formData.disability.ssiaward = (household_dataCopy.formData.disability.ssiaward==1) ? true : false 
+	}
+}
